136be20ee3199d5ff4383952a047efe4dbf4a70b
VARRTIX <zhao.wei@lebosemi.com>
2024-07-15 16:08:35 +0800

    update: pack cxx standard setting to marco 'set_stdcxx'

--- cmake/emu.cmake
+++ cmake/emu.cmake
@@ -10,10 +10,13 @@ if(ENABLE_EMU)
     message(STATUS "Enabling definition flag: DRV_EMU")
 endif()
 
-set(DRV_DEPS_INCLUDE ${CMAKE_CURRENT_LIST_DIR}/../include)
-message(STATUS "Using drivers deps. include: ${DRV_DEPS_INCLUDE}")
+get_filename_component(DRV_EMU_DIR ${CMAKE_CURRENT_LIST_DIR} DIRECTORY CACHE)
+set(DRV_DEPS_INCLUDE ${DRV_EMU_DIR}/include)
+message(STATUS "Using drv_emulator include: ${DRV_DEPS_INCLUDE}")
 
-set(CMAKE_CXX_STANDARD 17)
-set(CMAKE_CXX_STANDARD_REQUIRED TRUE)
-set(CMAKE_CXX_EXTENSIONS OFF)
+macro(set_stdcxx)
+    set(CMAKE_CXX_STANDARD 17)
+    set(CMAKE_CXX_STANDARD_REQUIRED TRUE)
+    set(CMAKE_CXX_EXTENSIONS OFF)
+endmacro()
 
